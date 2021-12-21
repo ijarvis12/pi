0a9ad52c716e59d10a51a1e8d2403877bd02a26f
Ian Jarvis <ijarvis12@gmail.com>
2021-12-21 15:50:23 -0600

    grunt work optimization

--- pihex.py
+++ pihex.py
@@ -8,17 +8,23 @@ for n in range(max_n):
 
 	for k in range(n+1):
 
+		nk = 16**(n-k)
+        	k1 = 8*k+1
+        	k4 = 8*k+4
+        	k5 = 8*k+5
+        	k6 = 8*k+6
+        	
 		summation += (
-			4*(16**(n-k)%(8*k+1))/(8*k+1)
-			- 2*(16**(n-k)%(8*k+4))/(8*k+4)
-			- (16**(n-k)%(8*k+5))/(8*k+5)
-			- (16**(n-k)%(8*k+6))/(8*k+6)
+			4*(nk % k1) / k1
+			- 2*(nk % k4) / k4
+			- (nk % k5) / k5
+			- (nk % k6) / k6
 		)
 
-	d = str(summation).split('.')[1]
+	digit = str(summation).split('.')[1]
 
-	d = hex(round(16*float('0.'+d)))[-1]
+	digit = hex(round(16*float('0.'+digit)))[-1]
 
-	print(d,end='',flush=True)
+	print(digit,end='',flush=True)
 
 print()
