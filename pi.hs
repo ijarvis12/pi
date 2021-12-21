344569167e0386d6dd90f0ff559a02a93abeb85f
Ian Jarvis <ijarvis12@gmail.com>
2021-12-20 22:48:53 -0600

    parenthesis

--- pi.hs
+++ pi.hs
@@ -4,10 +4,10 @@ import Data.BigDecimal
 
 forloop :: BigDecimal -> BigDecimal -> BigDecimal -> IO ()
 forloop n k value = do
-  let one1 = (BigDecimal 1 0)/(8*k+5)
-  let one2 = (BigDecimal 1 0)/(8*k+6)
-  let four = (BigDecimal 4 0)/(8*k+1)
-  let two = (BigDecimal 2 0)/(8*k+4)
+  let one1 = (BigDecimal 1 0)/((8*k)+5)
+  let one2 = (BigDecimal 1 0)/((8*k)+6)
+  let four = (BigDecimal 4 0)/((8*k)+1)
+  let two = (BigDecimal 2 0)/((8*k)+4)
   let hex = (BigDecimal 1 0)/(BigDecimal (16^(getValue k)) 0)
   let pi = (hex*(four-two-one1-one2)) + value
   if ((getValue n) == (getValue k)) then
