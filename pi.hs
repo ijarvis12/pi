e3a107ac620b273275ec3f085eb91fb16e80c5e8
Ian Jarvis <ijarvis12@gmail.com>
2022-11-12 15:51:13 -0600

    made more concise

--- pi.hs
+++ pi.hs
@@ -1,27 +1,25 @@
 #!/usr/bin/env runhaskell
 
 -- cabal install HasBigDecimal
-import Data.BigDecimal
+import Data.HasBigDecimal     
 
-forloop :: BigDecimal -> BigDecimal -> BigDecimal -> IO ()
-forloop n k value = do
-  let one1 = (BigDecimal 1 0)/((8*k)+5)
-  let one2 = (BigDecimal 1 0)/((8*k)+6)
-  let four = (BigDecimal 4 0)/((8*k)+1)
-  let two = (BigDecimal 2 0)/((8*k)+4)
-  let hex = (BigDecimal 1 0)/(BigDecimal (16^(getValue k)) 0)
+forloop :: BigDecimal -> BigDecimal -> BigDecimal -> Integer -> IO ()
+forloop n k pval 0 = putStrLn (toString p)
+forloop n k pval end = do
+  let one1 = (bigDecimal 1 0)/((8*k)+5)
+  let one2 = (bigDecimal 1 0)/((8*k)+6)
+  let four = (bigDecimal 4 0)/((8*k)+1)
+  let two = (bigDecimal 2 0)/((8*k)+4)
+  let hex = (bigDecimal 1 0)/(bigDecimal (16^(value k)) 0)
   -- Can't use pi b/c it's a constant in Prelude
-  let p = (hex*(four-two-one1-one2)) + value
-  if ((getValue n) == (getValue k)) then
-    putStrLn (toString p)
-  else
-    forloop n (k+1) p
+  let p = (hex*(four-two-one1-one2)) + pval
+  forloop n (k+1) p (end-1)
 
 main :: IO ()
 main = do
   putStrLn "Calculate Digits of Pi"
   putStrLn "Enter Number of Hexadecimal Digits (result is decimal):"
   i <- getLine
-  let n = BigDecimal (read i :: Integer) 0
-  let k = BigDecimal 0 0
-  forloop n k k
+  let n = bigDecimal (read i :: Integer) 0
+  let k = bigDecimal 0 0
+  forloop n k k (value n)
