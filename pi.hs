f1bc503ccf47f8022eece8a2f39c3914bb26da02
Ian Jarvis <ijarvis12@gmail.com>
2022-11-13 11:53:09 -0600

    update func name

--- pi.hs
+++ pi.hs
@@ -3,9 +3,9 @@
 -- cabal install HasBigDecimal
 import Data.BigDecimal
 
-forloop :: BigDecimal -> BigDecimal -> Integer -> IO ()
-forloop k pval 0 = putStrLn (toString pval)
-forloop k pval end = do
+forLoop :: BigDecimal -> BigDecimal -> Integer -> IO ()
+forLoop k pval 0 = putStrLn (toString pval)
+forLoop k pval end = do
   let one1 = (bigDecimal 1 0)/((8*k)+5)
   let one2 = (bigDecimal 1 0)/((8*k)+6)
   let four = (bigDecimal 4 0)/((8*k)+1)
@@ -13,7 +13,7 @@ forloop k pval end = do
   let hex = (bigDecimal 1 0)/(bigDecimal (16^(value k)) 0)
   -- Can't use pi b/c it's a constant in Prelude
   let p = (hex*(four-two-one1-one2)) + pval
-  forloop (k+1) p (end-1)
+  forLoop (k+1) p (end-1)
 
 main :: IO ()
 main = do
@@ -22,4 +22,4 @@ main = do
   i <- getLine
   let n = bigDecimal (read i :: Integer) 0
   let k = bigDecimal 0 0
-  forloop k k (value n)
+  forLoop k k (value n)
